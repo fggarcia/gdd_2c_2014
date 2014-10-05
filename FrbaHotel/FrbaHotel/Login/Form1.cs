545c1742fcc2246b31e0a9da57bab41976b867d2
Nicolas Fuentes <nicolasgfuentes@hotmail.com>
2014-10-05 16:41:47 -0300

    Creacion de la pantalla de login

--- FrbaHotel/FrbaHotel/Login/Form1.cs
+++ FrbaHotel/FrbaHotel/Login/Form1.cs
@@ -9,11 +9,17 @@ using System.Windows.Forms;
 
 namespace FrbaHotel.Login
 {
-    public partial class Form1 : Form
+    public partial class Form_Login : Form
     {
-        public Form1()
+        public Form_Login()
         {
             InitializeComponent();
         }
+
+        private void button_salir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
     }
 }
