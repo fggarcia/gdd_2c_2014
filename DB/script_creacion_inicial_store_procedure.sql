c87a0344eb76671d3694f7e093183343dc8c6bd6
Nicolas Fuentes <nicolasgfuentes@hotmail.com>
2014-10-26 03:22:42 -0300

    version mejorada del stored procedure para login de usuarios. Modularice el stored procedure, para que sea mas facil seguir la logica. Resta linkear con la aplicacion

--- DB/script_creacion_inicial_store_procedure.sql
+++ DB/script_creacion_inicial_store_procedure.sql
@@ -1,50 +1,77 @@
+USE GD2C2014
+GO
+
+create procedure SP_Login_Sucess @usuario varchar(20)
+	as
+	begin
+		update 	LA_MINORIA.Usuario 
+		set 	Cantidad_Login=0, 
+				Ultima_Fecha=SYSDATETIME()
+		where 	Id_Usuario=@usuario;
+	end
+go
+
+create procedure SP_retrieve_Login @usuario varchar(20), @cant int output
+	as
+	begin
+		
+		set @cant=(select Cantidad_Login
+						 from	LA_MINORIA.Usuario 
+						 where	Id_Usuario=@usuario)
+	end
+go
+
+create procedure SP_Login_Failed @user varchar (20)
+	as
+	begin
+		update LA_MINORIA.Usuario
+				set		Cantidad_Login=Cantidad_Login+1
+				where	Id_Usuario=@user
+				
+				declare @cant_login int
+				
+				exec SP_retrieve_Login @user,@cant_login output
+
+				if @cant_login=3
+					begin
+						update LA_MINORIA.Usuario
+						set		Habilitado=0	
+						where	Id_Usuario=@user
+					end
+	end
+go
+
 create procedure SP_Login_User @usuario varchar(20),@password varchar(64),@codigo int output
 as
 begin
-declare	@count int,	@passDB varchar(64),@cant_login int, @habilitado bit
+declare	@count int,	@passDB varchar(64), @habilitado bit
 	
-select @count=COUNT(Id_Usuario), 
+select @count=isnull(COUNT(Id_Usuario),0), 
 		@passDB=Password,
-		@cant_login=Cantidad_Login,
 		@habilitado=Habilitado 
 from	LA_MINORIA.Usuario
 where	Id_Usuario=@usuario
 group by Cantidad_Login,Habilitado,Password
 
 	
-	if (@count=0) /* si el usuario no existe, devuelvo 0*/
-		set @codigo=0;
-	else
-		begin
+	
 			if (@password=@passDB) 
 				begin
 					if (@habilitado=0) /* si esta inhabilitado, devuelvo 1*/
 						set @codigo=1 
 					else
+						 /*si los datos estan correctos, devuelvo 2, 
+						 borro los intentos fallidos y registro la fecha*/
 						begin
-							set @codigo=2; /*si los datos estan correctos, devuelvo 2, borro los 
-											intentos fallidos y registro la fecha*/
-							update LA_MINORIA.Usuario set Cantidad_Login=0, Ultima_Fecha=SYSDATETIME()
-							where Id_Usuario=@usuario;
+						exec SP_Login_Sucess @usuario
+						set @codigo=2
 						end
 				end
 			else
 				begin
-					set @codigo=3 /* si el usuario ya intento 3 veces, lo inhabilito*/
-					if @cant_login=3
-						begin
-							update LA_MINORIA.Usuario
-							set		Habilitado=0	
-							where	Id_Usuario=@usuario
-						end
-					else
-						begin /*si el usuario ingresa mal los datos, sumo 1 a la cantidad de intentos fallidos*/
-							update LA_MINORIA.Usuario
-							set		Cantidad_Login=Cantidad_Login+1
-							where	Id_Usuario=@usuario
-						end
+					exec SP_Login_Failed @usuario
+					set @codigo=3
 				end
 		end
-	end
 go
 
