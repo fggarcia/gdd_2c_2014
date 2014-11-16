4b91f5127e5a48ee9013296b214a46a1a0c87d8f
fedemv@gmail.com <F12icernall>
2014-11-16 04:58:44 -0300

    Agrego clase de Connection y Helpers para unificar funcionalidades

---
+++ FrbaHotel/FrbaHotel/Menu/Menu.cs
@@ -0,0 +1,8 @@
+﻿namespace FrbaHotel
+{
+    public class OpcionMenu
+    {
+        public int idFuncionalidad { get; set; }
+        public string descripcion { get; set; }
+    }
+}
---
+++ FrbaHotel/FrbaHotel/Menu/MenuHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using System.Collections.Generic;
+using System;
+namespace FrbaHotel
+{
+    public class MenuHelper
+    {
+        public static SortedList<int, OpcionMenu> getOptionMenu(int idRol)
+        {
+            SortedList<int, OpcionMenu> menuOptions = new SortedList<int, OpcionMenu>();
+
+            string storedProcedureName = "LA_MAYORIA.proc_listarMenuFuncionalidadPorRol";
+            SqlCommand command = new SqlCommand(storedProcedureName);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@p_idRol", idRol);
+
+            SqlDataReader reader = ProcedureHelper.execute(command) as SqlDataReader;
+
+            int position = 0;
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    OpcionMenu menuOption = new OpcionMenu();
+                    menuOption.descripcion = reader["descripcion"].ToString();
+                    menuOption.idFuncionalidad = Convert.ToInt32(reader["idFuncionalidad"]);
+                    menuOptions.Add(position, menuOption);
+                    position++;
+                }
+            }
+
+            return menuOptions;
+        }
+    }
+}
